4cadda45f758a711651476c6bb9f4957dc807d3f
Rui Chen <rchen47@ucsc.edu>
2020-04-12 13:42:13 -0700

    Commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 let config ={
-    tyoe: Phaser.CANVAS,
+    tyoe: Phaser.AUTO,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play]
 }
 
 let game = new Phaser.Game(config);
+
+//define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
+
+//reserve keyboard vars
+let keyF, keyLEFT, keyRIGHT;

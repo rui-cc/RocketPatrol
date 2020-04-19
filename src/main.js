c509e45d8043774bceb99394a860873c8030fd95
Rui Chen <rchen47@ucsc.edu>
2020-04-18 19:51:08 -0700

    comments updated

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+// Name: Rui Chen
+// Points Breakdown: Total-100
+//    (10)Add your own (copyright-free) background music to the Play scene
+//    (10)Allow the player to control the Rocket after it's fired
+//    (15)Replace the UI borders with new artwork
+//    (15)Create a new title screen
+//    (25)Create a new spaceship type (w/ new artwork) that's smaller, 
+//        moves faster, and is worth more points
+//    (25)Create new artwork for all of the in-game assets (rocket, 
+//        spaceships, explosion)
+// Music: Super Mario(Chinese Version) by a unknown musician.
+// New artworks are all drawn by myself.
+
 let config ={
     tyoe: Phaser.AUTO,
     width: 640,
